2c70198e29a5078c5421d96012ad356457ef7736
psch90 <pascalschuler1@web.de>
2016-06-02 20:04:03 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# =========================================================================
+'''
+*       Filename:  config.py
+*    Description:  configuration file for the CropScore Testsystem.
+*        Created:  2016-06-02 20:01
+*         Author:  (2016), Pascal Schuler
+*        Company:  da-cons GmbH, http://www.da-cons.de/
+*        Contact:  support@da-cons.de'''
+# =========================================================================
+
+bspTest      = 'example of a test', 4
+user      = 'root'
+password  = 'dumitri'
+dbname    = 'testing'
+imgsuffix = '*.png'
+
+# --- EOF -----------------------------------------------------------------
